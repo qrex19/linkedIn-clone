1908f86760c47f51f9071ac413ee37859243384a
qrex19 <nishanttjhaa25@gmail.com>
2023-10-23 14:04:08 +0530

    material ui imported and installed and header file creation initiated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+
+      <Header/>
+
+
+      {/* App body */}
+        {/* Side bar */}
+        {/* feed */}
+        {/* widjets */}
     </div>
   );
 }
